64523fb86a6f08cdeee0131d2ac90cebb8a9adf3
Archipe Sesanga <moisemozess@gmail.com>
2025-04-26 09:15:01 +0200

    deleted ann modified
     edited something in the ErrorModel and edited the index view

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -2,7 +2,6 @@ namespace StudentSync.Models;
 
 public class ErrorViewModel
 {
-    int Error { get; set; }
     public string? RequestId { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
