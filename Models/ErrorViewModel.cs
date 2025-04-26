04fed0d8ee7f50863bd6cf1dc3a3cf4e5aa6d07d
Archipe Sesanga <moisemozess@gmail.com>
2025-04-26 08:55:03 +0200

    ErrorModel REverted
    for teaching purpose

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace StudentSync.Models;
 
 public class ErrorViewModel
 {
-    int Error { get; set; }
+   
     public string? RequestId { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
