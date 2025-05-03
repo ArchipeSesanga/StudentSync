3850410fa154e99076558d21cb007eb80bd988ab
Archipe Sesanga <moisemozess@gmail.com>
2025-05-04 00:02:51 +0200

    E-commerce added

--- obj/Debug/net6.0/StudentSync.AssemblyInfo.cs
+++ obj/Debug/net6.0/StudentSync.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentSync")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+838bbb903b66a7ea9fc18577095dab2e2eeb711b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7be0d50675eabd523cfb8051a6f6e43e771b372")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentSync")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentSync")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
